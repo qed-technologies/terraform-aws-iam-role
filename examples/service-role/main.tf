d11263d3b827fcb1ae7a65308ba88f42760954f8
ChrisGreenwood <chris.greenwood3@genomicsengland.co.uk>
2022-12-05 11:57:54 +0000

    Add service based role example

---
+++ examples/service-role/main.tf
@@ -0,0 +1,19 @@
+module "iam_role" {
+  source = "../../"
+
+  create      = var.create
+  name        = "MyAwesomeIAMRole"
+  description = "IAM Role to do something"
+
+  trusted_services = [
+    "ecs.amazonaws.com"
+  ]
+
+  customer_managed_policies = [
+    {
+      name        = "MyAwesomeIAMRolePolicy"
+      description = "A customer managed IAM Policy document"
+      policy      = concat(data.aws_iam_policy_document.MyAwesomeIAMRolePolicy.*.json, [""])[0]
+    }
+  ]
+}
