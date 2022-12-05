f096a86c4a2d4c8c4e47b07d39e720f4d222827e
ChrisGreenwood <chris.greenwood3@genomicsengland.co.uk>
2022-12-05 08:38:24 +0000

    Add typical example

--- examples/typical-role/data.tf
+++ examples/typical-role/data.tf
@@ -1,22 +1,12 @@
 data "aws_iam_policy_document" "MyAwesomeIAMRolePolicy" {
   count = var.create ? 1 : 0
 
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:ListBuckets"]
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "MyPermissionsBoundary" {
-  count = var.create ? 1 : 0
-
   #checkov:skip=CKV_AWS_108:This is a demo policy
   #checkov:skip=CKV_AWS_109:This is a demo policy
   #checkov:skip=CKV_AWS_111:This is a demo policy
   statement {
     effect    = "Allow"
-    actions   = ["s3:*"]
+    actions   = ["s3:ListBuckets"]
     resources = ["*"]
   }
 }
